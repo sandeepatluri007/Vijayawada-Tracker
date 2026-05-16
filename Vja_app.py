7f8d6acfbe94a3aecf35277e835f798c6519fe44
sandeepatluri007 <sandeepatluri007@gmail.com>
2026-05-16 17:03:09 +0530

    Update Vja_app.py

--- Vja_app.py
+++ Vja_app.py
@@ -213,7 +213,7 @@ if not st.session_state["authenticated"]:
         login_btn = st.form_submit_button("Unlock Tracker", type="primary")
         
         if login_btn:
-            if pin_entry == "2333": 
+            if pin_entry == "1323": 
                 st.session_state["authenticated"] = True
                 st.success("Access Granted!")
                 st.rerun()
